0f574466faf63abbda5b67f0fe69b6b02a32a71b
Jing Liu <liujing92684@gmail.com>
2019-03-30 18:37:54 +1100

    add why did you update hook

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import MousePosition from './MousePosition';
+import Avatar from './Avatar/View';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Avatar/>
         <MousePosition/>
         </header>
       </div>
